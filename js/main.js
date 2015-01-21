812111162e2dd7a310514879b61830a8a973a1bc
christopherdumas <christopherdumas@me.com>
2015-01-21 14:14:15 -0800

    Fnished up carisoul/timeline-interactive thing, with effects and all. And also getting user's repos. No authentication as yet. also no actual viewing of events.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,78 @@
 var threads = {};
+var userInfo = {};
+var repos = [];
 
 $("#SI").click(function () {
-  $('.1').attr('class', 'col-md-3 col-md-offset-0 fade-in shadow-box 1 out');
-  $('.2').attr('class', 'col-md-4 col-md-offset-4 fade-in shadow-box 2');
-});
+  var ready = false;
+  var message = 'Something went wrong.';
 
-$('a.list-group-item').click(function () {
-  var repoName = $(this).children(".reponame").text();
-  $.ajax('https://api.github.com/repos/' + repoName).then(function (d) {
-    $.ajax(d.events_url).then(function (d) {
-      var issues_covered = [];
+  var email = $('#inputEmail3').val();
+  var password = $('#inputPassword3').val();
 
-      d.forEach(function (e) {
-        console.log(e);
-        if (e.type == "IssueCommentEvent") {
-          if (issues_covered.indexOf(e.id) != -1) {
-            threads[e.id].push(e.payload.comment.body);
-          } else {
-            issues_covered.push(e.id);
-            threads[e.id] = [e.payload.body];
-          }
-        }
+  if (!email && !password) message = 'You must supply your email and password!';
+  else if (!email) message = 'You must supply your email.';
+  else if (!password) message = 'You must supply your password.';
+  else ready = true;
+
+  if (ready) {
+    userInfo = {
+      email: email,
+      password: password,
+      username: email.split("@")[0]
+    };
+    $('.1').attr('class', 'col-md-3 col-md-offset-0 fade-in 1 out');
+    $('.2').attr('class', 'col-md-4 col-md-offset-4 fade-in shadow-box 2');
+    $('.3').attr('class', 'col-md-3 col-md-offset-10 fade-in out 3');
+
+    /*jshint multistr: true*/
+    $('.repos').html('\
+          <div class="progress">\
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>\
+          </div>\
+          <p class="text-muted">We are getting all your repos now...</p>\
+    ');
+
+    $.ajax('https://api.github.com/users/'+userInfo.username+'/subscriptions').then(function (d) {
+      repos = d;
+      $('.repos').html('');
+      repos.forEach(function (e) {
+        /*jshint multistr: true*/
+        $('.repos').append('\
+                           <a class="list-group-item repo">\
+                           <span class="reponame">'+e.full_name+'</span>\
+                           <i class="glyphicon glyphicon-chevron-right"></i>\
+                           </a>\
+                           ');
+      });
+      $('.repo').click(function () {
+        $('.1').attr('class', 'col-md-3 col-md-offset-0 fade-in shadow-box 1 hidden');
+        $('.2').attr('class', 'col-md-3 col-md-offset-0 fade-in 1 out');
+        $('.3').attr('class', 'col-md-4 col-md-offset-4 fade-in shadow-box 2');
+        var repoName = $(this).children(".reponame").text();
+
+        $('.3h1').text(repoName.split('/')[1] + '\'s Events');
+        $('.3h4').text('All of today\'s events from ' + repoName + '.');
+        $('.events').html('\
+              <div class="progress">\
+                <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>\
+              </div>\
+              <p class="text-muted">We are getting all your repo\'s events now...</p>\
+        ');
+
+        $.ajax('https://api.github.com/repos/' + repoName).then(function (d) {
+          $('.events').html('');
+          $.ajax(d.events_url).then(function (data) {
+
+          });
+        });
       });
     });
-  });
+
+  } else {
+    $('.alert-danger').remove();
+    $('.1').append('<div class="alert alert-danger" role="alert">'+ message +'</div>');
+    $('.alert-danger').hide();
+    $('.alert-danger').slideDown();
+  }
 });
+
